49e86ab1ebb4090dc829bdc02757d80359794ccc
storm <starovoyt_z@mail.ru>
2017-11-25 23:50:27 +0400

    Create cities_table migration

--- resources/lang/ru/validation.php
+++ resources/lang/ru/validation.php
@@ -1,10 +1,12 @@
 <?php
 return [
-    'email'=> 'Некорректный :attribute адрес.',
+    'email'                 => 'Некорректный :attribute адрес.',
+    'required'              => 'Поле :attribute не должно быть пустым.',
 
     'attributes' => [
         'email' => 'Email',
         'title' => 'Заголовок',
         'text'  => 'Текст',
+        'password' => 'Пароль'
     ],
 ];
